30e3f675852d168429be0496cd11092b499a33e1
itay <itayshapira@hotmail.com>
2017-05-29 00:09:20 +0300

    About Rename

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,6 +8,13 @@ var React = require('react');
 //   }
 // });
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+  return (
+  <div>
+    <h3>About Component</h3>
+    <p>Welcome to About page </p>
+  </div>
+)
+};
 
 module.exports = About;
